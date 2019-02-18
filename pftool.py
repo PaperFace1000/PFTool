17e334ef58503639f1ddbd94b1495daf2e71cbd6
paperface1000 <paperface1000eng@gmail.com>
2019-02-18 20:32:22 +0900

    パラメータ機能を実装開始

--- pftool.py
+++ pftool.py
@@ -1,6 +1,7 @@
 #!/bin/paython3
 
 import argparse
+from pathlib import Path
 
 class pftool:
     """PFTool
@@ -15,13 +16,24 @@ class pftool:
         Keyword Arguments:
             args {ArgumentParser} -- コマンドラインパラメータ用のパーサ (default: {None})
         """
+        # バージョン情報とか
         self.name = "PFTool"
         self.version = "0.01"
         self.about = ""
-        print("init")
-    
+
+        # パラメータ用パーサ
+        self.argparser = argparse.ArgumentParser(
+            prog=Path(__file__).name,
+            usage="コマンドラインから各種便利なCUIベースのツール郡です。\nこれはそのランチャーとしての役割を持ちます。",
+            description="test",
+            epilog='end',
+            add_help=True,
+        )
+        # TODO:descriptionの詳細な実装を
+
+
     def get_version(self):
-        """バージョン表示
+        """バージョン情報表示
         """
         dict = {
             "name":self.name,
@@ -29,11 +41,30 @@ class pftool:
             "about":self.about
         }
         return "{0[name]} {0[version]}\n{0[about]}".format(dict)
-        
 
+    def get_help(self):
+        """ヘルプ情報取得
+        """
+        # TODO: ヘルプ実装
+        return "help"
+
+    def get_modules(self):
+        """利用可能モジュール取得
+        """
+        # TODO: モジュール一覧実装
+        return "get_modules"
+        
 # メイン起動
 if __name__ == '__main__':
 
+    """
     obj = pftool()
     print(obj.get_version())
+
+    print(Path(__file__).name)
+    """
+
+    obj = pftool()
+    args = obj.argparser.parse_args()
+
 
